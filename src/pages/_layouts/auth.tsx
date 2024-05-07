e5136388fba6d93ab51557c5f7ebf3996b632530
elziorocha <rochasenzo.04@gmail.com>
2024-05-07 16:03:50 -0300

    branch commit - alterações visual do Auth 1

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,9 +1,11 @@
+import './auth.css'
+
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
     <div className="grid min-h-screen grid-cols-2">
-      <div className="flex h-full flex-col justify-between rounded-r-3xl  bg-muted bg-[url(/images/img-login.png)] p-10 text-muted-foreground"></div>
+      <div className="imagem-auth"></div>
       <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -9,7 +9,6 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
@@ -37,65 +36,69 @@ export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <div className="p-8 bg-custom-color bg-gradient rounded-xl">
-        <div className="flex w-[400px] flex-col justify-center gap-6">
-          <div className="flex flex-col gap-2">
-            <div className="mb-4 flex w-full md:mb-0 md:mr-4 md:w-auto">
-              <h1 className="text-4xl font-bold text-white">League of</h1>
-              <h1 className="ml-2 text-4xl font-bold text-blue-800">Draven</h1>
-            </div>
-            <p className="text-sm text-white">
-              Entre ou crie sua conta através do seu e-mail.
-            </p>
-          </div>
 
-          <Form {...form}>
-            <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
-              <div className="space-y-2">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Digite seu e-mail</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          type="email"
-                          placeholder="seuemail@email.com"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="space-y-2">
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Digite sua senha</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          type="password"
-                          placeholder="Informe sua senha"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              <Button className="w-full" type="submit">
-                Login
-              </Button>
-            </form>
-          </Form>
+      <div className="flex flex-col items-center justify-center gap-8">
+        <div className="mb-4 flex w-full md:mb-0 md:mr-4 md:w-auto">
+          <h1 className="text-4xl font-bold text-gray-700">League of</h1>
+          <h1 className="ml-2 text-4xl font-bold text-blue-700">Draven</h1>
         </div>
+
+        <Form {...form}>
+          <form
+            onSubmit={handleSubmit(handleSignIn)}
+            className="flex flex-col items-center space-y-4"
+          >
+            <div className="mb-4 w-96 space-y-2 p-0">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        placeholder="seuemail@email.com"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mb-4 w-96 space-y-2 p-0">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        placeholder="Informe sua senha"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <Button
+              className="mb-2 me-2 h-12 w-36 rounded-3xl border-2 border-black bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 px-5 py-2.5 text-center text-lg text-white hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
+              type="submit"
+            >
+              Login
+            </Button>
+
+            <div className="flex items-center text-sm">
+              <p className="text-black">Entre ou</p>
+              <p className="ml-1 font-bold text-blue-700">crie sua conta</p>
+              <p className="ml-1 text-black">através do seu email</p>
+            </div>
+          </form>
+        </Form>
       </div>
     </>
   )
