5895fddb3940a568233376df600001970a881395
elziorocha <rochasenzo.04@gmail.com>
2024-05-15 23:56:13 -0300

    layout inicial do dashboard

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -14,7 +14,7 @@ export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
-      <div className="flex">
+      <div className="flex flex-row">
         <SideBar>
           <SideBarItem
             icon={<Home color="#000" size={28} />}
@@ -48,7 +48,46 @@ export function Dashboard() {
             url="/ajuda"
           />
         </SideBar>
-        <div className="w-full bg-black">conteúdo</div>
+        <div className="mt-4 flex h-full w-full flex-col items-center">
+          <div className="flex flex-row items-center justify-center">
+            <div className="m-3 flex h-16 items-center justify-center bg-green-500 p-24">
+              teste
+            </div>
+            <div className="m-3 flex h-16 items-center justify-center bg-green-500 p-24">
+              teste
+            </div>
+            <div className="m-3 flex h-16 items-center justify-center bg-green-500 p-24">
+              teste
+            </div>
+            <div className="m-3 flex h-16 items-center justify-center bg-green-500 p-24">
+              teste
+            </div>
+          </div>
+
+          <div className="mt-6 flex w-full flex-col items-center justify-items-start">
+            <div className="m-3 flex h-48 w-5/6 items-center justify-center bg-green-500 p-16">
+              teste2
+            </div>
+            <div className="m-3 flex h-48 w-5/6 items-center justify-center bg-green-500 p-16">
+              teste2
+            </div>
+          </div>
+
+          <div className="mt-4 flex flex-row items-center justify-center">
+            <div className="m-3 flex w-56 items-center justify-center bg-green-500 py-40">
+              teste
+            </div>
+            <div className="m-3 flex w-56 items-center justify-center bg-green-500 py-40">
+              teste
+            </div>
+            <div className="m-3 flex w-56 items-center justify-center bg-green-500 py-40">
+              teste
+            </div>
+            <div className="m-3 flex w-56 items-center justify-center bg-green-500 py-40">
+              teste
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
